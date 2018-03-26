78a702a87228aad751898b77a0c68b95d9e3e644
Rafael Gibam <rafael.w.silva@hotmail.com>
2018-03-26 12:55:17 -0300

    Implementando CRUD do GARCOM

--- Views/cozinheiro/funcionarios.php
+++ Views/cozinheiro/funcionarios.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once __DIR__ . "/../../autoload.php";
 
 if(!isset($_SESSION['cpf'])){
     header("location: /../index.php");
@@ -9,12 +10,48 @@ if(isset($_GET['action'])){
     session_destroy();
     header("location: /../index.php");
 }
+$con = \Infra\Conexao::getConnection();
+$sql = "SELECT * FROM GARCOM;";
+$stmt = $con->prepare($sql);
+
 ?>
 <?php include __DIR__ . "/../layout/head.php"; ?>
 <?php include __DIR__ . "/../layout/menucozin.php"; ?>
-
-
-    <h1 class="text-center">Funcionarios</h1>
-
-
+    <div class="container-fluid">
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">CPF</th>
+                <th scope="col">NOME</th>
+                <th scope="col">SEXO</th>
+                <th scope="col">DATA NASCIMENTO</th>
+                <th scope="col">NÚMERO FIXO</th>
+                <th scope="col">NÚMERO CELULAR</th>
+                <th scope="col">ESTADO</th>
+                <th scope="col">GOJETA</th>
+                <th scope="col">DATA ADMISSÃO</th>
+                <th scope="col">SALÁRIO</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+                if($stmt->execute()):
+                    while ($linha = $stmt->fetch()):
+            ?>
+            <tr>
+                <th scope="row"><?= $linha->CPF ?></th>
+                <td><?= $linha->NOME ?></td>
+                <td><?= $linha->SEXO = "M" ? "Masculino" : "Feminino" ?></td>
+                <td><?= $linha->DATA_NASC ?></td>
+                <td><?= $linha->NUM_FIXO ?></td>
+                <td><?= $linha->NUM_CELULAR ?></td>
+                <td><?= $linha->ESTADO = 1 ? "Ativo" : "Inativo" ?></td>
+                <td><?= $linha->GOJETA . "%" ?></td>
+                <td><?= $linha->DATA_ADMISSAO ?></td>
+                <td><?= "R$" . $linha->SALARIO ?></td>
+            </tr>
+            <?php endwhile;  endif; ?>
+            </tbody>
+        </table>
+    </div>
 <?php include __DIR__ . "/../layout/footer.php"; ?>
--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@ define('WWW_ROOT', dirname( __FILE__ )); // Constante pega o caminho do arquivo 
 define('DS', DIRECTORY_SEPARATOR ); // Constante pega a " / " do caminho do arquivo
 
 function __autoload( $class ){
-    $class = WWW_ROOT . DS . str_replace('\\' , DS , $class) . '.php'; // Inverte as barras para contra barras " \ " e pega o caminho da classe
+    $class = WWW_ROOT . DS . str_replace('\\' , DS , $class) . '.php'; // Inverte as barras para contra barras " \Class " e pega o caminho da classe
 
     if(!file_exists( $class )){ // testa se o arquivo $class conseguiu localizar a classe no diretorio.
         throw new Exception("O Arquivo: {$class} não existe");
