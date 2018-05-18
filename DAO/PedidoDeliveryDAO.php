21407d7940691c4c4f89fc0a81f05a2e39192588
Rafael Willames <rafael.w.silva@hotmail.com>
2018-05-17 22:33:12 -0300

    Implementando PedidoDeliveryDAO

---
+++ DAO/PedidoDeliveryDAO.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace DAO;
+
+
+class PedidoDeliveryDAO extends Model implements ICrud
+{
+    protected $table = "pedido";
+
+    public function insert($obj)
+    {
+        $stmt = DB::getCon()->prepare("INSERT INTO {$this->table} (`NUMERO`,`NUMERO`,`COR`,`QTD_LUGAR`,
+                                                `PESO`,`ALTURA`,`COMPRIMENTO`,`LARGURA`,`ESTADO`)
+                                                 VALUES (?,?,?,?,?,?,?,?)");
+
+        $stmt->bindValue(1, $obj->getNome());
+        $stmt->bindValue(2, $obj->getNumero());
+        $stmt->bindValue(3, $obj->getCor());
+        $stmt->bindValue(4, $obj->getCapacidade());
+        $stmt->bindValue(5, $obj->getPeso());
+        $stmt->bindValue(6, $obj->getAltura());
+        $stmt->bindValue(7, $obj->getComprimento());
+        $stmt->bindValue(8, $obj->getLargura());
+        $stmt->bindValue(9, $obj->getEstado());
+
+        $stmt->execute();
+        $stmt->closeCursor();
+    }
+
+    public function update($obj)
+    {
+        // TODO: Implement update() method.
+    }
+}
