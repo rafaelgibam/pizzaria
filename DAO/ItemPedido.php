4876f5c36271e98bcfa25582b83c46b621acb7a3
Rafael Willames <rafael.w.silva@hotmail.com>
2018-05-19 13:43:39 -0300

    Implementando ItemPedidoDAO

---
+++ DAO/ItemPedido.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafaelgibam
+ * Date: 19/05/2018
+ * Time: 13:42
+ */
+
+namespace DAO;
+
+
+class ItemPedido extends Model implements ICrud
+{
+
+    public function insert($obj)
+    {
+        // TODO: Implement insert() method.
+    }
+
+    public function update($obj)
+    {
+        // TODO: Implement update() method.
+    }
+}
