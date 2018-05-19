44ea0e0360d1ebd9ec7e9304f2078a6ed0f19c53
Rafael Willames <rafael.w.silva@hotmail.com>
2018-05-19 14:05:09 -0300

    ClienteController Implementado!

--- Controllers/ClienteController.php
+++ Controllers/ClienteController.php
@@ -2,10 +2,51 @@
 
 namespace Controllers;
 
-use Models\BaseModel;
-use stdClass;
+use DAO\ClienteDAO;
+use Models\Cliente;
+use Models\Endereco;
 
-class ClienteController extends BaseModel
+class ClienteController
 {
-   
+    public function insert($cpf, $rg, $nome, $sexo, $datanasc, $numfixo, $numcelular, $estado, $endereco){
+        // ID	CPF	RG	NOME	SEXO	DATA_NASC	NUM_FIXO	NUM_CELULAR	ESTADO	CRIADO_EM	ENDERECO_ID
+        $c = new Cliente();
+        $cdao = new ClienteDAO();
+
+        $e = new Endereco();
+        $e->setId($endereco);
+
+        $c->setCpf($cpf);
+        $c->setNome($nome);
+        $c->setSexo($sexo);
+        $c->setDtnasc($datanasc);
+        $c->setNumfixo($numfixo);
+        $c->setNumcel($numcelular);
+        $c->setEstado($estado);
+        $c->setEndereco($e);
+
+        $cdao->insert($c);
+
+    }
+
+    public function update($id, $cpf, $rg, $nome, $sexo, $datanasc, $numfixo, $numcelular, $estado, $endereco){
+        $c = new Cliente();
+        $cdao = new ClienteDAO();
+
+        $e = new Endereco();
+        $e->setId($endereco);
+
+        $c->setId($id);
+        $c->setCpf($cpf);
+        $c->setRg($rg);
+        $c->setNome($nome);
+        $c->setSexo($sexo);
+        $c->setDtnasc($datanasc);
+        $c->setNumfixo($numfixo);
+        $c->setNumcel($numcelular);
+        $c->setEstado($estado);
+        $c->setEndereco($e);
+
+        $cdao->update($c);
+    }
 }
