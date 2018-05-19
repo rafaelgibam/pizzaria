08e97b5a6df5647514db63120c92e859c5144375
Rafael Willames <rafael.w.silva@hotmail.com>
2018-05-19 13:58:37 -0300

    EnderecoController Implementado!

--- Controllers/EnderecoController.php
+++ Controllers/EnderecoController.php
@@ -1,17 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rafaelgibam
- * Date: 06/04/2018
- * Time: 11:11
- */
 
 namespace Controllers;
 
+use DAO\EnderecoDAO;
 use Models\Endereco;
-
 
 class EnderecoController
 {
-    
+    public function insert($logradouro, $numero, $complemento, $bairro, $municipio, $uf, $pais, $referencia, $cep){
+        $e = new Endereco();
+        $edao = new EnderecoDAO();
+
+        $e->setLogradouro($logradouro);
+        $e->setNumero($numero);
+        $e->setComplemento($complemento);
+        $e->setBairro($bairro);
+        $e->setMunicipio($municipio);
+        $e->setUf($uf);
+        $e->setPais($pais);
+        $e->setReferencia($referencia);
+        $e->setCep($cep);
+
+        $edao->insert($e);
+    }
+
+    public function update($id ,$logradouro, $numero, $complemento, $bairro, $municipio, $uf, $pais, $referencia, $cep){
+        $e = new Endereco();
+        $edao = new EnderecoDAO();
+        $e->setId($id);
+        $e->setLogradouro($logradouro);
+        $e->setNumero($numero);
+        $e->setComplemento($complemento);
+        $e->setBairro($bairro);
+        $e->setMunicipio($municipio);
+        $e->setUf($uf);
+        $e->setPais($pais);
+        $e->setReferencia($referencia);
+        $e->setCep($cep);
+
+        $edao->update($e);
+    }
 }
